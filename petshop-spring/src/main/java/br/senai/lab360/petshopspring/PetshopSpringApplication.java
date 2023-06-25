ef7d38f8958ee13cfe52d8fd97fd063abb9fa16c
Pedro Alencar <pedromariutti@gmail.com>
2023-06-25 17:51:53 -0300

    Exercício 04

---
+++ petshop-spring/src/main/java/br/senai/lab360/petshopspring/PetshopSpringApplication.java
@@ -0,0 +1,13 @@
+package br.senai.lab360.petshopspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopSpringApplication.class, args);
+	}
+
+}
